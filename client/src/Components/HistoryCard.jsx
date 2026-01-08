f520d8a2397319dabcd2e1fdca8913bd13bcc416
sadman-sakib-ASG <sadman@asgshop.com.bd>
2026-01-08 12:26:20 +0600

    Fixed the card with for long url

--- client/src/Components/HistoryCard.jsx
+++ client/src/Components/HistoryCard.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const HistoryCrad = ({item}) => {
+const HistoryCrad = ({ item }) => {
   const shortUrl = `${import.meta.env.VITE_API}/${item.alias}`;
   return (
-    <div className="bg-white shadow-lg rounded-lg p-4 flex justify-between items-center">
-      <div className="overflow-hidden">
-        <p className="font-medium truncate">{item.originalUrl}</p>
-        <a href={shortUrl} className="text-blue-600 text-sm">{shortUrl}</a>
+
+    <div className="bg-white shadow-lg rounded-lg p-4 flex items-center gap-4 w-full max-w-3xl overflow-hidden">
+
+      <div className="min-w-0 flex-1">
+        <p className="font-medium text-slate-800 truncate break-all">
+          {item.originalUrl}
+        </p>
+
+        <a className="text-blue-600 text-sm truncate block break-all">
+          {shortUrl}
+        </a>
       </div>
 
       <Link
         to={`/stats/${item.alias}`}
-        className="text-sm border px-3 py-1.5 rounded-lg hover:bg-slate-100"
+        className="shrink-0 text-sm border px-4 py-2 rounded-lg whitespace-nowrap"
       >
-        View details
+        Details →
       </Link>
     </div>
+
+
+
   );
 };
 
